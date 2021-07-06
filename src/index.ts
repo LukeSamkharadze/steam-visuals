e026aa066896819a546b88e992acd734dd605936
Luka Samkharadze <lukesamkharadze@gmail.com>
2021-07-06 16:57:52 +0400

    add dockerfile

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-console.log("Hello");
+import express from "express";
 
-const a = 3;
+const app = express();
+const port = 8080;
+
+app.get("/", (request, respond) => {
+  respond.send("Hello world!");
+});
+
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
